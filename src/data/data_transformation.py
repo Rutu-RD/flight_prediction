e8ae37d68e445ea7707c809670fa9c6062d9281f
Ruturaj Desai <ruturajdesai@Ruturajs-MacBook-Air.local>
2025-12-03 23:56:09 -0500

    added changes to transformation to split files into x_train,val and test also for y

--- src/data/data_transformation.py
+++ src/data/data_transformation.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 import logging
+from sklearn.model_selection import train_test_split
+from yaml import safe_load
 
 logging.basicConfig(level=logging.INFO)
 console = logging.StreamHandler()
@@ -16,9 +18,14 @@ def transformation(df: pd.DataFrame) -> pd.DataFrame:
        .assign(arrival_min=lambda x: x['arr_time'].dt.minute)
        .assign(flight_day=lambda x: x['date_of_journey'].dt.day)
        .assign(flight_month=lambda x: x['date_of_journey'].dt.month)
-       .assign(flight_year=lambda x: x['date_of_journey'].dt.year)
+       
        )
-    
+
+def spliiting_data(df:pd.Dataframe)-> tuple[pd.DataFrame, pd.DataFrame]:
+    logger.info("splitting data into X and y")
+    X = df.drop(columns=['price'])
+    y = df['price']
+    return X, y   
 if __name__ == "__main__":
     logger.info("data transformation starting")
     
@@ -26,9 +33,24 @@ if __name__ == "__main__":
     
     df.info()
     transformed_df=transformation(df)
+    
     print("transformed_df_info")
     #transformed_df.info()
     data_path = os.path.join("data", "transformed")
     os.makedirs(data_path, exist_ok=True)
     transformed_df.to_csv(os.path.join("data","transformed","transformed_flight_price.csv"), index=False)    
     logger.info("data transformation completed")
+    logger.info("data splitting starting")
+    X, y = spliiting_data(transformed_df)
+    with open("params.yaml") as f:
+        params = safe_load(f)
+    test_size = params['test_size']
+    x_, x_test, y_, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    logger.info("data splitting completed")
+    
+    x_train, x_val, y_train, y_val = train_test_split(x_, y_, test_size=test_size, random_state=42)
+    os.makedirs(os.path.join("data","split"), exist_ok=True)
+    x_train.to_csv(os.path.join("data","split","X_train.csv"), index=False)
+    x_val.to_csv(os.path.join("data","split","X_val.csv"), index=False)
+    y_train.to_csv(os.path.join("data","split","y_train.csv"), index=False)
+    y_val.to_csv(os.path.join("data","split","y_val.csv"), index=False)
