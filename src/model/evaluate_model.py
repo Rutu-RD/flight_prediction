f410706ac3164955fc1e414a876b9e3b75d93351
Ruturaj Desai <ruturajdesai@Ruturajs-MacBook-Air.local>
2025-12-07 20:41:42 -0500

    made changes to train_model and added train_model_rf,train_model_xgboost

--- src/model/evaluate_model.py
+++ src/model/evaluate_model.py
@@ -11,26 +11,49 @@ logging.basicConfig(level=logging.INFO)
 console = logging.StreamHandler()
 logger = logging.getLogger(__name__)
 logger.addHandler(console)
+import mlflow
+mlflow.set_tracking_uri("http://localhost:5000")
+
 if __name__ == "__main__":
-    logger.info("model evaluation starting")
+    mlflow.set_experiment("flight_price_prediction model evaluation")
+    with mlflow.start_run(run_name="random_forest_model_evaluation"):
+        logger.info("model evaluation starting")
 
-    x_test = pd.read_csv(os.path.join("data", "splitted_data", "X_val.csv"))
-    y_test = pd.read_csv(os.path.join("data", "splitted_data", "y_val.csv"))
+        x_test = pd.read_csv(os.path.join("data", "splitted_data", "X_val.csv"))
+        y_test = pd.read_csv(os.path.join("data", "splitted_data", "y_val.csv"))
 
-    if isinstance(y_test, pd.DataFrame) and y_test.shape[1] == 1:
-        y_test = y_test.iloc[:, 0]
+        if isinstance(y_test, pd.DataFrame) and y_test.shape[1] == 1:
+            y_test = y_test.iloc[:, 0]
 
-    model_pipeline: Pipeline = joblib.load(os.path.join("models", "random_forest_model.pkl"))
+        model_pipeline: Pipeline = joblib.load(os.path.join("models", "random_forest_model.pkl"))
+        logger.info("random forest model loaded successfully")
+        y_pred = model_pipeline.predict(x_test)
 
-    y_pred = model_pipeline.predict(x_test)
+        mae = mean_absolute_error(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
 
-    mae = mean_absolute_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+        logger.info(f"Mean Absolute Error: {mae}")
+        logger.info(f"Mean Squared Error: {mse}")
+        logger.info(f"R^2 Score: {r2}")
+        mlflow.log_metric("MAE", mae)
+        mlflow.log_metric("MSE", mse)
+        mlflow.log_metric("R2_Score", r2)
+        logger.info("RF model evaluation completed")
+#    with mlflow.start_run(run_name="xgboost_model_evaluation"):
 
-    logger.info(f"Mean Absolute Error: {mae}")
-    logger.info(f"Mean Squared Error: {mse}")
-    logger.info(f"R^2 Score: {r2}")
 
-
-    logger.info("model evaluation completed")
+ #       logger.info(" xgboost model evaluation starting")
+ #       model_pipeline: Pipeline = joblib.load(os.path.join("models", "xgboost_model.pkl"))
+ #       logger.info("xgboost model loaded successfully")
+ #       y_pred = model_pipeline.predict(x_test)
+ #       mae = mean_absolute_error(y_test, y_pred)
+ #       mse = mean_squared_error(y_test, y_pred)
+  #      r2 = r2_score(y_test, y_pred)
+ #       logger.info(f"Mean Absolute Error: {mae}")
+ #       logger.info(f"Mean Squared Error: {mse}")
+ #       logger.info(f"R^2 Score: {r2}")
+  #      logger.info("xgboost model evaluation completed")
+  #      mlflow.log_metric("MAE", mae)
+   #     mlflow.log_metric("MSE", mse)
+   #     mlflow.log_metric("R2_Score", r2)   
---
+++ src/model/train_model_xgboost.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import os
+import logging
+from sklearn.pipeline import Pipeline
+from xgboost import XGBRegressor
+from yaml import safe_load
+from src.model.pipeline import build_preprocessor 
+import joblib
+
+logging.basicConfig(level=logging.INFO)
+console = logging.StreamHandler()
+logger = logging.getLogger(__name__)
+logger.addHandler(console)
+
+if __name__=="__main__":
+    logger.info("training xgboost regressor model")
+
+    x_train = pd.read_csv(os.path.join("data", "splitted_data", "X_train.csv"))
+    y_train = pd.read_csv(os.path.join("data", "splitted_data", "y_train.csv"))
+
+    if isinstance(y_train, pd.DataFrame) and y_train.shape[1] == 1:
+        y_train = y_train.iloc[:, 0]
+
+    with open("params.yaml") as f:
+        params = safe_load(f)
+    n_estimators = params['model']['xgboost']['n_estimators']
+    max_depth = params['model']['xgboost']['max_depth']
+    learning_rate = params['model']['xgboost']['learning_rate']
+    random_state = params['model']['xgboost']['random_state']
+    
+    xgb = XGBRegressor(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        learning_rate=learning_rate,
+        random_state=random_state
+        )
+    model_pipeline = Pipeline(steps=[
+        ('preprocessor', build_preprocessor()),
+        ('model', xgb)
+    ])
+    model_pipeline.fit(x_train, y_train)
+    logger.info("xgboost model training completed")
+    os.makedirs("models", exist_ok=True)
+    joblib.dump(model_pipeline, os.path.join("models", "xgboost_model.pkl"))
+    logger.info("xgboost model saved successfully")
